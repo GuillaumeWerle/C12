edd18aa1cff53dbc8d0a9a6546fa96ef5341e81d
GuillaumeWerle <guillaume.werle.77@gmail.com>
2017-10-05 09:50:47 +0200

    .

--- C12/DXTexture2D.cpp
+++ C12/DXTexture2D.cpp
@@ -83,8 +83,17 @@ void DXTexture2D::LoadDDS(const FileSystem::Path & path)
 	desc.SampleDesc.Count = 1;
 	desc.SampleDesc.Quality = 0;
 	desc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	m_desc = desc;
 
-	Create(desc);
+	DX::Device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		nullptr,
+		IID_PPV_ARGS(&m_resource));
+
+	m_srv.Create(m_resource.Get(), nullptr);
 
 	CreateUploadBuffer();
 
@@ -121,6 +130,14 @@ void DXTexture2D::CreateDepthStencil(u32 width, u32 height)
 		&clearValue,
 		IID_PPV_ARGS(&m_resource));
 
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvdesc = {};
+	srvdesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvdesc.Format = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+	srvdesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvdesc.Texture2D.MipLevels = 1;
+	srvdesc.Texture2D.MostDetailedMip = 0;
+	srvdesc.Texture2D.PlaneSlice = 0;
+	srvdesc.Texture2D.ResourceMinLODClamp = 0.0f;
 }
 
 void DXTexture2D::CopyScratchImageToUploadBuffer(DirectX::ScratchImage &scratch)
